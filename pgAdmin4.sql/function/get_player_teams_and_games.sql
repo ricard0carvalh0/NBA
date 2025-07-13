6bdc59ee5110fcc32fc4bbe5ec013bd480860111
Ricardo Ribeiro <ricardo.r.carvalho2001@gmail.com>
2025-07-13 12:04:44 -0300

    #8 - Adicionando função get_player_teams_and_games

---
+++ pgAdmin4.sql/function/get_player_teams_and_games.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION get_player_teams_and_games (fname TEXT, lname TEXT)
+RETURNS TABLE (
+	team_id INT,
+	team_name TEXT,
+	total_games INT
+) AS $$ 
+BEGIN
+	RETURN QUERY
+	SELECT 
+		pb.player_team_id AS team_id,
+		t.city || ' ' || t.nickname AS team_name, 
+		COUNT(DISTINCT pb.game_id) AS total_games 
+	FROM player_info p
+	JOIN play_by_play pb ON pb.player_id = p.id
+	JOIN team t ON t.id = pb.player_team_id
+	WHERE p.first_name = fname AND p.last_name = lname
+	GROUP BY pb.player_team_id, t.city, t.nickname;
+END
+$$ LANGUAGE plpgsql;
+
+-- Documentação:
+--  Essa é uma função que recebe como parâmetro o nome e sobrenome de um jogador e retorna uma tabela com todos os times pelos quais ele atuou ao longo da carreira, assim como a quantidade total de jogos que ele disputou por cada um desses times;
+--  Foi utilizado o comando RETURN QUERY para executar e retornar diretamente os dados de uma consulta, funcionando como uma espécie de "mini view";
+--  Há também a concatenação de t.city com t.nickname de team para formar o nome do time que o jogador atuou, como por exemplo "Oklahoma City Thunder".
